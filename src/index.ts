28dd8dc24449f7b297ef3073195a59643f19e157
kimromi <kimromi4@gmail.com>
2019-06-12 09:24:57 +0900

    Assign reviewers!

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 import { Application } from 'probot' // eslint-disable-line no-unused-vars
 
+interface AppConfig {
+  reviewers: string[]
+}
+
 export = (app: Application) => {
   app.on('pull_request.opened', async (context) => {
-    const prComment = context.issue({ body: 'Thanks for opening this pull request!' })
-    await context.github.issues.createComment(prComment)
+    const config: AppConfig = await context.config<AppConfig>('reviewers_assign.yml', { reviewers: [] })
+
+    const reviewers = context.issue({ reviewers: config.reviewers })
+    await context.github.pullRequests.createReviewRequest(reviewers)
   })
 }
