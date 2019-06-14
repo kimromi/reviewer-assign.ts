5038a85d4f37da902750b4a95d8504da50d5adf0
kimromi <kimromi4@gmail.com>
2019-06-14 16:56:38 +0900

    random reviewers assign

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,26 @@
 import { Application } from 'probot' // eslint-disable-line no-unused-vars
+import _ from 'underscore'
 
 interface AppConfig {
   reviewers: string[]
+  reviewersNumber?: number
 }
 
 export = (app: Application) => {
   app.on('pull_request.opened', async (context) => {
     const config: AppConfig = await context.config<AppConfig>('reviewers_assign.yml', { reviewers: [] })
+    console.log
 
-    const reviewers = context.issue({ reviewers: config.reviewers })
+    const owner = context.payload.pull_request.user.login
+    let filteredReviewers = config.reviewers.filter((reviewer: string): boolean => {
+        return reviewer !== owner
+      }
+    )
+    if (config.reviewersNumber) {
+      filteredReviewers = _.sample(filteredReviewers, config.reviewersNumber)
+    }
+
+    const reviewers = context.issue({ reviewers: filteredReviewers })
     await context.github.pullRequests.createReviewRequest(reviewers)
   })
 }
