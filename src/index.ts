c2982501363d0ddc64f0bf931b0036098f5b6d68
kimromi <kimromi4@gmail.com>
2019-06-21 16:06:23 +0900

    Add label option

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,57 @@
-import { Application } from 'probot' // eslint-disable-line no-unused-vars
+import { Application, Context } from 'probot' // eslint-disable-line no-unused-vars
 import _ from 'underscore'
 
-interface AppConfig {
+interface ReviewersConfig {
   reviewers: string[]
   reviewersNumber?: number
 }
 
-export = (app: Application) => {
-  app.on('pull_request.opened', async (context) => {
-    const config: AppConfig = await context.config<AppConfig>('reviewers_assign.yml', { reviewers: [] })
+interface LabelConfig extends ReviewersConfig {
+  label: string
+}
 
-    const owner = context.payload.pull_request.user.login
-    let filteredReviewers = config.reviewers.filter((reviewer: string): boolean => {
-        return reviewer !== owner
-      }
-    )
-    if (config.reviewersNumber) {
-      filteredReviewers = _.sample(filteredReviewers, config.reviewersNumber)
+interface AppConfig {
+  open?: ReviewersConfig
+  labels?: LabelConfig[]
+}
+
+export = (app: Application) => {
+  app.on('pull_request.opened', async (context: Context) => {
+    const config: AppConfig = await context.config<AppConfig>('reviewers_assign.yml',)
+    if (!config.open) {
+      return
     }
 
-    const reviewers = context.issue({ reviewers: filteredReviewers })
-    await context.github.pullRequests.createReviewRequest(reviewers)
+    await createReviewRequest(context, config.open)
   })
+
+  app.on('pull_request.labeled', async (context) => {
+    const config: AppConfig = await context.config<AppConfig>('reviewers_assign.yml')
+    if (!config.labels) {
+      return
+    }
+
+    const addedLabel = context.payload.label.name
+
+    config.labels.forEach(async (labelConfig) => {
+      if (labelConfig.label === addedLabel) {
+        await createReviewRequest(context, labelConfig)
+      }
+    })
+  })
+}
+
+async function createReviewRequest(context: Context, config: ReviewersConfig) {
+  const owner = context.payload.pull_request.user.login
+
+  let filteredReviewers = config.reviewers.filter((reviewer: string): boolean => {
+    return reviewer !== owner
+  }
+  )
+  if (config.reviewersNumber) {
+    filteredReviewers = _.sample(filteredReviewers, config.reviewersNumber)
+  }
+
+  const reviewers = context.issue({ reviewers: filteredReviewers })
+  await context.github.pullRequests.createReviewRequest(reviewers)
 }
